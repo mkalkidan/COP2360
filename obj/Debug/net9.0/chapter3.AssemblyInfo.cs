6b62e1300a0a8916efc06ea1d373515d434757e1
Kalkidan <mkalkidany@gmail.com>
2025-07-13 23:59:53 -0400

    stock class

--- obj/Debug/net9.0/chapter3.AssemblyInfo.cs
+++ obj/Debug/net9.0/chapter3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chapter3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9252727c597a28ada20785563295a823bd5109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb672f532ecfbfdbf2b056cc7a3df43bde95158")]
 [assembly: System.Reflection.AssemblyProductAttribute("chapter3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chapter3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
