5cb672f532ecfbfdbf2b056cc7a3df43bde95158
Kalkidan <mkalkidany@gmail.com>
2025-07-13 23:25:23 -0400

    The stock class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Stock myStock = new Stock();             // Create an object
+        myStock.CurrentPrice = 20.5m;            // Set price
+        myStock.SharesOwned = 10;                // Set shares
+
+        Console.WriteLine("Current Price: " + myStock.CurrentPrice);
+        Console.WriteLine("Shares Owned: " + myStock.SharesOwned);
+        Console.WriteLine("Total Worth: " + myStock.Worth);
+    }
+}
